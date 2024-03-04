05fcf8048685f2ff95aea6b7436473fa74141948
retr0reg <patrick_0429@163.com>
2024-03-04 23:30:28 +0800

    Uploading

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zer0poc',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='PoC generater thats fast and pretty for your vulnerabilty.',
+    long_description=open('README.md').read(),
+    url='https://github.com/retr0reg/zer00poc',
+    author='0reg',
+    author_email='0reg@0reg.dev'
+)
