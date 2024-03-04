f93a2484c9dee5171cba2c7cfc25d5e5004ad647
retr0reg <patrick_0429@163.com>
2024-03-04 23:39:38 +0800

    Uploading

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='zer0poc',
     version='0.1',
     packages=find_packages(),
+    install_requires=requirements,
     license='MIT',
     description='PoC generater thats fast and pretty for your vulnerabilty.',
     long_description=open('README.md').read(),
